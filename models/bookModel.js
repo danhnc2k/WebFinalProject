1a8ecfc9ff3542a0235a4d7dff759183b45ff196
ducnghu <55783340+ducnghu@users.noreply.github.com>
2020-11-16 19:42:18 +0700

    chỉnh sửa trang bị lặp

--- models/bookModel.js
+++ models/bookModel.js
@@ -6,17 +6,5 @@ exports.list = () => {
             cover: 'https://picsum.photos/200/300',
             basePrice: 18000
         },
-        {
-            id: 2,
-            title: 'Harry Potter',
-            cover: 'https://picsum.photos/200/300',
-            basePrice: 180000
-        },
-        {
-            id: 3,
-            title: 'Tiếng Việt 1 tập 1',
-            cover: 'https://picsum.photos/200/300',
-            basePrice: 10000
-        },
-    ]
+        ]
 }
